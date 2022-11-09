1d96e1e8d26d8d4cb4d4e22f38135a81863f055f
Salmalisa <salmalisa7342@gmail.com>
2022-11-09 15:10:14 +0600

    my review page added

---
+++ src/Components/MyReviewRow.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const MyReviewRow = ({ review }) => {
+  const {serviceTitle,email,photoURL,message,currentDate}=review
+  return (
+    <div >
+      <tr className='border'>
+        <th>
+          <label title='Delete' className='p-1 bg-base-200 rounded cursor-pointer'>
+            ❌
+          </label>
+        </th>
+        <div className='grid grid-cols-12'>
+        <td className='col-span-4'>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-circle rounded w-32 h-32 ">
+                <img src={photoURL} alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{serviceTitle}</div>
+                <div className="text-sm opacity-50">{currentDate}</div>
+            </div>
+          </div>
+        </td>
+        <td className=' col-span-5  flex items-center'>
+            <span>
+            {message}
+        </span>
+        </td>
+        <td className='col-span-2 flex items-center'><span>{email}</span></td>
+        <th className='col-span-1 flex items-center'>
+          <button className="bg-gradient-to-br from-pink-300 to-teal-200 py-1 px-5 rounded">Edit</button>
+        </th>
+       </div>
+      </tr>
+  
+    </div>
+  );
+};
+
+export default MyReviewRow;
--- src/Pages/MyReviews.js
+++ src/Pages/MyReviews.js
@@ -1,19 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { useContext } from 'react';
-import { AuthContext } from '../Contexts/AuthProvider';
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
+import MyReviewRow from "../Components/MyReviewRow";
+import { AuthContext } from "../Contexts/AuthProvider";
 
 const MyReviews = () => {
   const [reviews, setReviews] = useState([]);
-  const {user}=useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   useEffect(() => {
-    fetch(`http://localhost:5000/reviews?email=${user.email}`)
+    fetch(`http://localhost:5000/reviews?email=${user?.email}`)
       .then((res) => res.json())
       .then((data) => setReviews(data));
-  }, [user.email]);
+  }, [user?.email]);
 
   return (
     <div>
-      
+      <h1 className="text-4xl text-blue-900 font-semibold my-12 text-center">
+        You made <span className="text-pink-600">{reviews.length} </span>reviews
+        to <span className="text-pink-600">Lara Cripton</span>
+      </h1>
+      <div className="overflow-x-auto w-11/12 mx-auto ">
+        <table className="table w-full">
+          <tbody className="border">
+            {reviews.map((review) => (
+              <MyReviewRow key={review._id} review={review}></MyReviewRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
